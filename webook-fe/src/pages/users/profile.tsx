748779d77bd5f955464361de5da9cbd4058ffaaa
zlilemon <330478270@qq.com>
2023-10-08 09:44:20 +0800

    add  feature

---
+++ webook-fe/src/pages/users/profile.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import axios from "@/axios/axios";
+
+function Page() {
+    type Profile = {
+        Email: string
+    }
+    let p: Profile = {Email: ""}
+    const [data, setData] = useState<Profile>(p)
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axios.get('/users/profile')
+            .then((res) => res.data)
+            .then((data) => {
+                setData(data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
+
+    return (
+        <div>
+            <h1>{data.Email}</h1>
+            {/*<p>{data.bio}</p>*/}
+        </div>
+    )
+}
+
+export default Page
